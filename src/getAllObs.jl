929d829da70dbb6e10193ddcfb75c02d35d30152
Dave Marchant <dwfmarchant@gmail.com>
2017-02-28 12:02:36 -0800

    Changed getAllObs to work for AbstractMesh.

--- src/getAllObs.jl
+++ src/getAllObs.jl
@@ -3,11 +3,9 @@ export getAllObs
 
 #--------------------------------------------------------------------------
 
-function getAllObs( trx::Array{TransmitterOmega},
-                    M::OcTreeMesh )
-    # Calculate interpolation matrices from fields (on all edges) to receivers.
+function getAllObs( trx::Array{TransmitterOmega}, M::AbstractMesh )
 
-    nEx,nEy,nEz = getEdgeNumbering(M)
+    # Calculate interpolation matrices from fields (on all edges) to receivers.
 
     const mu0 = 4*pi*1e-7
     nedges = sum( M.ne )
@@ -24,7 +22,7 @@ function getAllObs( trx::Array{TransmitterOmega},
         iwmu = complex(0.0, 1.0 / (mu0 * omega))
        
         for ii = 1:nrcv
-            Receivers[:,ii] = complex(getEdgeIntegralOfPolygonalChain(M,Recs[ii],nEx,nEy,nEz,normalize=true))
+            Receivers[:,ii] = complex(getEdgeIntegralOfPolygonalChain(M,Recs[ii],normalize=true))
 
             if norm(Recs[ii][1,:] - Recs[ii][end,:]) < 1e-16
                 # for closed loops, scale by i/(omega*mu0)
