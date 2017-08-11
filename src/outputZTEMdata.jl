21ac693351801b31f7927cc7a5f058cfaac80a46
Roman Shekhtman <shekht@eos.ubc.ca>
2017-08-11 14:49:24 -0700

    stuff

--- src/outputZTEMdata.jl
+++ src/outputZTEMdata.jl
@@ -1,5 +1,5 @@
 
-export outputFieldZTEMdata, outputZTEMdata
+export outputFieldZTEMdata, outputZTEMdata, dumpZTEM
 
 
 function outputFieldZTEMdata( filename::String, trx::TransmitterOmega, DD::Vector{Complex128} )
@@ -8,9 +8,12 @@ function outputFieldZTEMdata( filename::String, trx::TransmitterOmega, DD::Vecto
     fdata = open(filename, "w")
 
     for id = 1:3:length(DD)
-        xx = mean( trx.Recs[id][:,1] )
-        yy = mean( trx.Recs[id][:,2] )
-        zz = mean( trx.Recs[id][:,3] )
+        mn,mx = extrema( trx.Recs[id][:,1] )
+        xx = ( mn + mx ) / 2.0
+        mn,mx = extrema( trx.Recs[id][:,2] )
+        yy = ( mn + mx ) / 2.0
+        mn,mx = extrema( trx.Recs[id][:,3] )
+        zz = ( mn + mx ) / 2.0
         @printf(fdata,
         "%15.7e %15.7e %15.7e   %13.5e %13.5e  %13.5e %13.5e  %13.5e %13.5e\n",
         xx, yy, zz,
@@ -21,7 +24,7 @@ function outputFieldZTEMdata( filename::String, trx::TransmitterOmega, DD::Vecto
     close(fdata)
 
     return
-end
+end  # function outputFieldZTEMdata
 
 #--------------------------------------------------------------------------
 
@@ -33,15 +36,75 @@ function outputZTEMdata(filename::String, trx::TransmitterOmega, DD::Array{Compl
 
     fdata = open(filename, "w")
     for id = 1:nr
-        xx = mean( trx.Recs[id*3-2][:,1] )
-        yy = mean( trx.Recs[id*3-2][:,2] )
-        zz = mean( trx.Recs[id*3-2][:,3] )
+        ir = id*3-2
+        mn,mx = extrema( trx.Recs[ir][:,1] )
+        xx = ( mn + mx ) / 2.0
+        mn,mx = extrema( trx.Recs[ir][:,2] )
+        yy = ( mn + mx ) / 2.0
+        mn,mx = extrema( trx.Recs[ir][:,3] )
+        zz = ( mn + mx ) / 2.0
+
         @printf(fdata, 
-        "%15.7e %15.7e %15.7e   %13.5e %13.5e  %13.5e %13.5e\n",
-        xx, yy, zz,
-        real(zdpred[1,id]), imag(zdpred[1,id]),
-        real(zdpred[2,id]), imag(zdpred[2,id]) )
+              "%15.7e %15.7e %15.7e   %13.5e %13.5e  %13.5e %13.5e\n",
+              xx, yy, zz,
+        real(zdpred[2,id]), imag(zdpred[2,id]),
+        real(zdpred[1,id]), imag(zdpred[1,id]) )
     end
     close(fdata)
+    
 return
-end
+end  # function outputZTEMdata
+
+#--------------------------------------------------------------------------
+
+function outputZTEMdataNOxyz(fdata::IOStream,
+                              DD::Array{Complex128,2})
+    # Output the ZTEM data.   
+
+    zdpred = calcZTEMdata(DD)
+    nr = size(zdpred,2)
+
+   # fdata = open(filename, "w")
+    for id = 1:nr
+        @printf(fdata, 
+        "%13.5e %13.5e  %13.5e %13.5e\n",
+        real(zdpred[2,id]), imag(zdpred[2,id]),
+        real(zdpred[1,id]), imag(zdpred[1,id]) )
+    end
+   # close(fdata)
+   
+return
+end  # function outputZTEMdataNOxyz
+
+#--------------------------------------------------------------------------
+
+using jInv.ForwardShare.interpGlobalToLocal
+using JOcTree
+
+function dumpZTEM( mc::Array{Float64,1},
+                   Dc::Array{Future,1},
+                   iter::Int64,
+                   pInv,   #::jInv.InverseSolve.InverseParam,
+                   pMis::Array{RemoteChannel,1}) #    ::MisfitParam )
+
+    Mis = fetch(pMis[1])
+       
+    sigt, = pInv.modelfun(mc)
+    sigma,dsigma = Mis.modelfun(sigt)
+    sigmaloc = interpGlobalToLocal(sigma,Mis.gloc.PForInv,Mis.gloc.sigmaBackground)
+
+    modfile = @sprintf("m_%02i.con", iter)
+    exportUBCOcTreeModel(modfile, pInv.MInv, sigmaloc)
+
+    fdata = open("pred.txt", "w")
+
+    for i = 1:length(Dc)
+       dc = fetch(Dc[i])
+       println(fdata)
+       outputZTEMdataNOxyz(fdata, dc )
+    end
+
+    close(fdata)
+
+    return
+end  # function dumpZTEM
--- src/readMTdata.jl
+++ src/readMTdata.jl
@@ -1,5 +1,5 @@
 
-export readMTdata
+export readMTdata, readZTEMdata
 
 
 function readMTdata(filename::String,
@@ -52,15 +52,11 @@ function readMTdata(filename::String,
          end  # j
          
          zdata[2,2,ir] = complex( dobs[1], dobs[2] )
-       #  zdata[1,2,ir] = complex( dobs[3], dobs[4] )
-       #  zdata[2,1,ir] = complex( dobs[5], dobs[6] )
          zdata[2,1,ir] = complex( dobs[3], dobs[4] )
          zdata[1,2,ir] = complex( dobs[5], dobs[6] )
          zdata[1,1,ir] = complex( dobs[7], dobs[8] )
          
          zWd[2,2,ir] = complex( sd[1], sd[2] )
-       #  zWd[1,2,ir] = complex( sd[3], sd[4] )
-       #  zWd[2,1,ir] = complex( sd[5], sd[6] )
          zWd[2,1,ir] = complex( sd[3], sd[4] )
          zWd[1,2,ir] = complex( sd[5], sd[6] )
          zWd[1,1,ir] = complex( sd[7], sd[8] )
@@ -76,3 +72,70 @@ function readMTdata(filename::String,
    return trx
 end  #function readMTdata
 
+#---------------------------------------------------------------------------
+
+function readZTEMdata(filename::String,
+                      trx::Array{TransmitterOmega})
+   
+   ntrx = length(trx)   
+   fieldsPerData = 3
+
+   ncomp = 2*2  #  # of components
+   ignore = -99
+
+   dobs = Array{Float64}(ncomp)
+   sd   = Array{Float64}(ncomp)
+   
+   fdata = open(filename, "r")
+
+   for itx = 1:ntrx
+   
+      ndata = length(trx[itx].Recs)
+      nr = div(ndata, fieldsPerData)
+   
+      zdata = Array{Complex128}(2,nr)
+      zWd   = Array{Complex128}(2,nr)
+   
+      for ir = 1:nr
+         line = skipcmnts(fdata)
+         if length(line) != 3+2*ncomp
+            error("length(line) != 3+2*ncomp")
+         end
+   
+         xx = parse(Float64,line[1])
+         yy = parse(Float64,line[2])
+         zz = parse(Float64,line[3])
+   
+         for j = 1 : ncomp
+            jj = j*2 - 1
+            dobs[j] = parse(Float64,line[3+jj])
+              sd[j] = parse(Float64,line[3+jj+1])
+
+            if dobs[j] == ignore
+               dobs[j] = 0.0  # dummy value
+               sd[j]   = 0.0
+
+            elseif sd[j] <= 0
+               println(line)
+               error("standard deviation is negative.")
+            else
+               sd[j] = 1.0 / sd[j]
+            end
+         end  # j
+         
+         zdata[2,ir] = complex( dobs[1], dobs[2] )
+         zdata[1,ir] = complex( dobs[3], dobs[4] )
+         
+         zWd[2,ir] = complex( sd[1], sd[2] )
+         zWd[1,ir] = complex( sd[3], sd[4] )
+         
+      end  # ir
+      
+      trx[itx].Dobs = zdata
+      trx[itx].Wd = zWd
+
+   end  # itx
+   close(fdata)
+      
+   return trx
+end  #function readZTEMdata
